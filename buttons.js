64f493695237efbfd8b0304f903478b80d28fee0
Sacye <dondensace25@gmail.com>
2025-03-15 00:27:12 +0800

    Delete buttons.js

--- buttons.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".cta-button");
-
-    buttons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Button clicked! Navigating...");
-        });
-    });
-});
